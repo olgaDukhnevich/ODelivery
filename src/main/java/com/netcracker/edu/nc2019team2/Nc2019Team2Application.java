e15f489ad3030fba5e688ba5cb34b3c01e100de3
Newbie9999 <PRn218CS6>
2019-10-18 00:43:32 +0300

    initial commit

---
+++ src/main/java/com/netcracker/edu/nc2019team2/Nc2019Team2Application.java
@@ -0,0 +1,13 @@
+package com.netcracker.edu.nc2019team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nc2019Team2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nc2019Team2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/netcracker/edu/nc2019team2/Nc2019Team2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcracker.edu.nc2019team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nc2019Team2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
