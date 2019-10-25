ac7273adadb84ebb6c7d386b5c2962bf10cc2d23
DobrevOleg <oleg.dobrev.99@mail.ru>
2019-10-25 15:57:25 +0300

    Тест

--- src/main/java/com/netcracker/edu/nc2019team2/Nc2019Team2Application.java
+++ src/main/java/com/netcracker/edu/nc2019team2/Nc2019Team2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Nc2019Team2Application {
-
+//Проверка на коммиты
 	public static void main(String[] args) {
 		SpringApplication.run(Nc2019Team2Application.class, args);
 	}
