b026bca2d98ed57f93c2e375b666aa81e978d079
Newbie9999 <PRn218CS6>
2019-10-29 23:58:51 +0200

    model classes

--- src/main/java/com/netcracker/edu/nc2019team2/delivery/database/entity/Client.java
+++ src/main/java/com/netcracker/edu/nc2019team2/delivery/database/entity/Client.java
@@ -6,29 +6,9 @@ import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.Date;
 
-public class Client extends Entity {
-    private long id;
-    private String lastName;
+public class Client extends Person {
     private String defaultLocation;
-    private String email;
-    private byte rating;
     private Date registrationDate;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 
     public String getDefaultLocation() {
         return defaultLocation;
@@ -36,22 +16,6 @@ public class Client extends Entity {
 
     public void setDefaultLocation(String defaultLocation) {
         this.defaultLocation = defaultLocation;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public byte getRating() {
-        return rating;
-    }
-
-    public void setRating(byte rating) {
-        this.rating = rating;
     }
 
     public Date getRegistrationDate() {
